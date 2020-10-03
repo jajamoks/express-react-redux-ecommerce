888aa5dd5eb86386044317699cf3faa3ab8731c2
jajamoks <jaja_aldrin@yahoo.com>
2020-10-03 23:54:30 +0800

    add ES Module support node14 version

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,8 @@
-const express = require('express')
-const dotenv = require('dontenv')
-const products = require('./data/products')
-const app = express()
-
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
 dotenv.config()
-
+const app = express()
 app.get('/', (req, res) => {
   res.send('API is running')
 })
